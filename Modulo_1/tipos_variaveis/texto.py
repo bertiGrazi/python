4ca2714ac2b523426e0a399a4c37150c4e51d070
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-24 21:41:13 -0300

    texto

---
+++ Modulo_1/tipos_variaveis/texto.py
@@ -0,0 +1,7 @@
+# Formatação 
+nome_completo = "Grazielli Lima Berti"
+
+print("Nome completo(1a forma): ", nome_completo)
+print("Nome completo(2a forma): " + nome_completo)
+print("Nome completo(3a forma): %s" % nome_completo)
+print(f"Nome completo(4a forma): {nome_completo}")
