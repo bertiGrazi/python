4a3eb3b962827288ece8944fe0603a17d44a9e3b
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-24 21:02:11 -0300

    sintaxe

---
+++ Modulo_1/blocos.py
@@ -0,0 +1,8 @@
+idade = 27 
+
+if idade < 18: 
+  print("menor de 18 anos")
+else:
+    print("maior de 18 anos")
+
+    
---
+++ Modulo_1/sintaxe.py
@@ -0,0 +1,9 @@
+# Esse é meu primeiro comentário 
+
+""""
+    Comentário de múltiplas linhas
+
+"""
+
+nome = "Grazielli" + "Berti"
+
