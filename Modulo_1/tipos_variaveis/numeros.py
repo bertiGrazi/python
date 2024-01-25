4bedd3088cfd4a3a3c28e3e6c59e9dca43b9c205
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-24 21:25:52 -0300

    numeros e variaveis

---
+++ Modulo_1/tipos_variaveis/numeros.py
@@ -0,0 +1,45 @@
+# Inteiro
+numero_inteiro = 3
+print("Inteiro: ", numero_inteiro)
+
+# Real com ponto flutuante
+numero_real = 3.14
+print("Real com Ponto Flutuante: ", numero_real)
+
+# type() = Retorna o tipo da variável que estamos trabalhando
+print("Tipo da variável que estamos trabalhando: ", type(numero_inteiro)) #int
+print("Tipo da variável que estamos trabalhando: ", type(numero_real)) #float
+
+# soma + 
+
+soma = 1 + 1
+print("1 + 1 =",soma)
+
+# subtração
+subtracao = 1 - 1
+print("1 - 1 =", subtracao)
+
+# multiplicação
+multiplicacao = 2 * 2
+print("2 x 2 = ", multiplicacao)
+
+# divisao
+divisao = 5 / 2
+print("5 / 2 = ", divisao)
+print("Tipo da variável que estamos trabalhando: ", type(divisao)) #float
+
+# converter para inteiro
+int()
+print("Valor em inteiro: ", int(divisao))
+
+# converter para float
+float()
+print("Valor em float: ", float(soma))
+
+# modulo = mostra o restante da nossa divisao
+modulo = 5 % 2 
+print("Resto da divisão de 5 / 2 : ", modulo)
+
+# divisao por inteiro
+divisao_inteiro = 5 // 2
+print("Divisão por inteiro de 5 // 2 : ", divisao_inteiro)
---
+++ Modulo_1/variaveis.py
@@ -0,0 +1,12 @@
+# Armazenar dados de forma simples
+
+# Recomendado para variaveis
+nome_completo = "Gazielli Lima Berti"
+idade = 27
+
+# Recomendado para classes - camelcase
+dataDeNascimento = "25/02/1996"
+
+print(nome_completo)
+print(idade)
+print(dataDeNascimento)
