5eb7039ea115e6add45c1b7c70cab42deb6cd650
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-29 13:35:49 -0300

    main string methods in python

---
+++ Modulo_1/tipos_variaveis/principaisMetodos.py
@@ -0,0 +1,28 @@
+# Mostra quandos a tem na variavel nome. 
+# nome.count("a")
+
+# Encontrar a posição
+# nome.find("a")
+
+# Converter nossa string para Bytes (Muito utilizado em string)
+# nome.encode()
+
+# Decotificar 
+# nome.encode().decode()
+
+# Substituindo - Muito utilizado para tratamento de dados. 
+# telefone.replace("-", "")
+
+# Separar informações 
+# "-".join(nome)
+# 'J-e-r-o-n-i-m-a'
+
+# Remover o que está no começo e no final
+# nome.strip("x")
+
+# Remove o que está a direita na String 
+# nome.rstrip("x")
+
+# Verificar se a variável retorna com o que você está esperando
+# nome_completo.startswith("Gr")
+# "ll" in nome_completo 
