a32688e60f653c82d97339246909127c3baf7c86
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-30 13:37:59 -0300

    dictionary in python

---
+++ Modulo_1/tipos_variaveis/dicionario.py
@@ -0,0 +1,15 @@
+#  Dicionários: Coleção NÃO ordenada de pares (chave: valor)
+
+# Criando um dicionário de exemplo
+pessoa = {"nome": "Grazielli", "idade": 27, "cidade": "Guarulhos"}
+print("Meu dicionário: ", pessoa)
+
+# Acessando valores por chaves
+print("Nome: ", pessoa["nome"])
+print("Idade: ", pessoa["idade"])
+print("Cidade: ", pessoa["cidade"])
+
+# Atribuindo valor a um dicionário que já foi criado
+pessoa["sobrenome"] = "Berti"
+print("Sobrenome: ", pessoa["sobrenome"])
+
