0b5acce6d09b84e160007dbe784ddd0de360c887
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-30 16:22:33 -0300

    create def for update and list task in python

---
+++ Modulo_1/tipos_variaveis/gerenciador.py
@@ -0,0 +1,52 @@
+def adicionar_tarefa(tarefas, nome_tarefa="tarefa padrão"):
+    # tarefa: nome da tarefa
+    # completada: indicar se essa tarefa já foi completada ou não
+    tarefa = {"tarefa":nome_tarefa, "completada":False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+    return 
+
+def ver_tarefas(tarefas):
+   print("\n Lista de tarefas: ")
+   for indice, tarefa in enumerate(tarefas, start=1):
+      status = "✓" if tarefa["completada"] else " "
+      nome_tarefa = tarefa["tarefa"]
+      print(f"{indice}. [{status}] {nome_tarefa}")
+   return 
+
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+  indice_tarefa_ajustado = int(indice_tarefa) - 1
+  if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+    tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
+    print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+  else:
+     print("Indíce de tarefa inválido!\n")
+  return 
+
+tarefas = []
+
+while True: 
+  print("\nMenu do Gerenciador de Lista de tarefas: ")
+  print("1. Adicionar tarefa")
+  print("2. Ver tarefa")
+  print("3. Atualizar tarefa")
+  print("4. Completar tarefa")
+  print("5. Deteltar tarefas completadas.")
+  print("6. Sair.")
+
+  escolha = input("Digite a sua escolha: ")
+
+  if escolha == "1":
+     nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+     adicionar_tarefa(tarefas, nome_tarefa=nome_tarefa)
+  elif escolha == "2":
+     ver_tarefas(tarefas)
+  elif escolha == "3":
+     ver_tarefas(tarefas)
+     indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
+     novo_nome_tarefa = input("Digite o novo nome da tarfea: ")
+     atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa)
+  elif escolha == "6":
+    break
+
+  print("\nPrograma finalizado!!!")
---
+++ Modulo_1/tipos_variaveis/while.py
@@ -0,0 +1,6 @@
+contador = 0 
+while contador < 5:
+  print("Contagem: ", contador)
+  contador += 1 # para não dar o loop infinito
+  
+
