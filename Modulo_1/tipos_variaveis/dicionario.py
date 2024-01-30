a1e744fdeb2a88a81760d7f54dbe412bfcb1b8fa
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-30 13:52:22 -0300

    methods dictionary in python

--- Modulo_1/tipos_variaveis/dicionario.py
+++ Modulo_1/tipos_variaveis/dicionario.py
@@ -13,3 +13,25 @@ print("Cidade: ", pessoa["cidade"])
 pessoa["sobrenome"] = "Berti"
 print("Sobrenome: ", pessoa["sobrenome"])
 
+# Atualizando dados
+pessoa["idade"] = 28
+print(pessoa)
+
+# Removendo um par chave:valor
+del pessoa["sobrenome"]
+print(pessoa)
+
+# Método keys()
+chaves = list(pessoa.keys())
+print("Chaves do dicionários: ", chaves)
+print("Primeira chave: ", chaves[0])
+
+# Método values()
+valores = list(pessoa.values())
+print("Valores do dicionário: ", valores)
+print("Primeiro valor do dicionário: ", valores[0])
+
+# Métodos items = Cada cheve valor vira uma tupla
+itens = list(pessoa.items())
+print("Pares chave-valor do dicionário: ", itens)
+print("Primeiro chave-valor: %s = %s " % (itens[0][0], itens[0][1]))
